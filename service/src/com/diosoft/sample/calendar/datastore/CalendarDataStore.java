d06c994ec9bb8bbf71a44ce817890f71960af41b
Vitalii <vs@intellisoftware.net>
2015-10-06 22:54:38 +0300

    Fixed compilation error

--- service/src/com/diosoft/sample/calendar/datastore/CalendarDataStore.java
+++ service/src/com/diosoft/sample/calendar/datastore/CalendarDataStore.java
@@ -2,13 +2,11 @@ package com.diosoft.sample.calendar.datastore;
 
 import com.diosoft.sample.calendar.common.Event;
 
-import java.util.List;
-
 public interface CalendarDataStore {
 
     void publish(Event event);
 
     Event remove(String eventName);
 
-    List<Event> getEvent(String name);
+    Event getEvent(String name);
 }
